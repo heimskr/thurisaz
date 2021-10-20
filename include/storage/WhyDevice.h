b74ea1e41e54584d61c30237f8872bfdc398acfa
Kai Tamkun <ktamkun@ucsc.edu>
2021-10-20 16:28:07 -0700

    Add WhyDevice

---
+++ include/storage/WhyDevice.h
@@ -0,0 +1,8 @@
+#include "storage/StorageDevice.h"
+
+struct WhyDevice: StorageDevice {
+	const int index;
+	WhyDevice(int index_);
+	ssize_t read(void *buffer, size_t bytes, size_t byte_offset) override;
+	ssize_t write(const void *buffer, size_t bytes, size_t byte_offset) override;
+};
---
+++ src/storage/WhyDevice.cpp
@@ -0,0 +1,44 @@
+#include "storage/WhyDevice.h"
+
+WhyDevice::WhyDevice(int index_): index(index_) {}
+
+ssize_t WhyDevice::read(void *buffer, size_t bytes, size_t byte_offset) {
+	// We're going to be messing with the argument registers, so we have to save the arguments into something else in
+	// case the arguments are optimized to live only in the argument registers or something like that.
+	void * const buffer_copy = buffer;
+	const size_t bytes_copy = bytes, byte_offset_copy = byte_offset;
+
+	long e0, r0;
+
+	asm("%0 -> $a1 \n %1 -> $a2" :: "r"(index), "r"(byte_offset_copy));
+	asm("<io seekabs>");
+	asm("$e0 -> %0" : "=r"(e0));
+	if (e0 != 0)
+		return -e0;
+
+	asm("%0 -> $a1 \n %1 -> $a2 \n %2 -> $a3" :: "r"(index), "r"(buffer_copy), "r"(bytes_copy));
+	asm("<io read>");
+	asm("$e0 -> %0 \n $r0 -> %1" : "=r"(e0), "=r"(r0));
+
+	return e0 != 0? -e0 : r0;
+}
+
+ssize_t WhyDevice::write(const void *buffer, size_t bytes, size_t byte_offset) {
+	// Same here.
+	const void * const buffer_copy = buffer;
+	const size_t bytes_copy = bytes, byte_offset_copy = byte_offset;
+
+	long e0, r0;
+
+	asm("%0 -> $a1 \n %1 -> $a2" :: "r"(index), "r"(byte_offset_copy));
+	asm("<io seekabs>");
+	asm("$e0 -> %0" : "=r"(e0));
+	if (e0 != 0)
+		return -e0;
+
+	asm("%0 -> $a1 \n %1 -> $a2 \n %2 -> $a3" :: "r"(index), "r"(buffer_copy), "r"(bytes_copy));
+	asm("<io write>");
+	asm("$e0 -> %0 \n $r0 -> %1" : "=r"(e0), "=r"(r0));
+
+	return e0 != 0? -e0 : r0;
+}
