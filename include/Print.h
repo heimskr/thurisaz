3d479fd3e412a6726d0b187b685acece9a39865b
Kai Tamkun <ktamkun@ucsc.edu>
2024-12-31 19:02:50 -0800

    There's no epilogue to save us in naked functions anymore

--- include/Print.h
+++ include/Print.h
@@ -1,19 +1,19 @@
 #pragma once
 
 inline void __attribute__((naked, always_inline)) prd(long) {
-	asm("<prd $a0>");
+	asm("<prd $a0>; : $rt");
 }
 
 inline void __attribute__((naked, always_inline)) prc(char) {
-	asm("<prc $a0>");
+	asm("<prc $a0>; : $rt");
 }
 
 inline void __attribute__((naked, always_inline)) prx(long) {
-	asm("<prx $a0>");
+	asm("<prx $a0>; : $rt");
 }
 
 inline void __attribute__((naked, always_inline)) prb(long) {
-	asm("<prb $a0>");
+	asm("<prb $a0>; : $rt");
 }
 
 extern "C" void strprint(const char *str);
