4bfe1f8067aa2cbd47ed0218861b903c0f9f1bf2
Kai Tamkun <ktamkun@ucsc.edu>
2024-12-31 19:03:12 -0800

    Let's do strprint in extra.wasm instead of a naked C++ function

--- src/Print.cpp
+++ src/Print.cpp
@@ -1,13 +1,13 @@
 #include "Print.h"
 
-extern "C" void __attribute__((naked)) strprint(const char *) {
-	asm("@_strprint_loop          \
-	     [$a0] -> $ma /b          \
-	     : _strprint_print if $ma \
-	     : _strprint_done         \
-	     @_strprint_print         \
-	     <prc $ma>                \
-	     $a0 + 1 -> $a0           \
-	     : _strprint_loop         \
-	     @_strprint_done; <>");
-}
+// extern "C" void __attribute__((naked)) strprint(const char *) {
+// 	asm("@_strprint_loop          \
+// 	     [$a0] -> $ma /b          \
+// 	     : _strprint_print if $ma \
+// 	     : _strprint_done         \
+// 	     @_strprint_print         \
+// 	     <prc $ma>                \
+// 	     $a0 + 1 -> $a0           \
+// 	     : _strprint_loop         \
+// 	     @_strprint_done; : $rt");
+// }
